bca18b4d29f5bf68aea80e41645a45af2c8cd3f6
divyashah98 <ddsdivya.98@gmail.com>
2017-05-22 21:41:20 +0530

    Add files via upload

---
+++ Exceptional handling/nested try-catch/Divide.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class Divide
+{
+public static void main(String args[]) throws IOException
+{
+int a=0,b=0,res;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str;
+System.out.println("enter two numbers:");
+
+try
+{
+str=br.readLine();
+a=Integer.parseInt(str);
+str=br.readLine();
+b=Integer.parseInt(str);
+
+try
+{
+res=a/b;
+
+}
+catch(ArithmeticException ae)
+{
+System.out.println("exception has occured. you have entered the divisor as zero ");
+}
+
+
+
+}
+
+catch(NumberFormatException ne)
+{
+System.out.println("invalid number");
+}
+
+
+
+}
+}
